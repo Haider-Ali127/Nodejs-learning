97e393c4604b50be6ac60f4278b5d64582599d43
Haider Ali <haidet0000@gmail.com>
2025-12-02 15:43:41 +0500

    task9 todo.js done

---
+++ task9/todo.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let todos = [];
+let nextId = 1;
+app.post("/todo", (req, res) => {
+    const { title, description, status } = req.body;
+    if (!title) {
+        return res.status(400).json({ status: "error", message: "where is title" });
+    }
+    if (status !== "pending" && status !== "completed") {
+        return res.status(400).json({ status: "error", message: "status is  invalid" });
+    }
+    const newTodo = {
+        id: nextId++,
+        title,
+        description: description || "",
+        status: status || "pending"
+    };
+    todos.push(newTodo);
+    res.status(201).json({
+        status: "success",
+        todo: newTodo
+    });
+});
+app.get("/todo", (req, res) => {
+    res.json({
+        status: "success",
+        todos
+    });
+});
+app.get("/todo/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const todo = todos.find(t => t.id === id);
+
+    if (!todo) {
+        return res.status(404).json({ status: "error", message: "Todo not found" });
+    }
+
+    res.json({ status: "success", todo });
+});
+app.put("/todo/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const todo = todos.find(t => t.id === id);
+
+    if (!todo) {
+        return res.status(404).json({ status: "error", message: "your todo list was not found" });
+    }
+    todo.title = req.body.title || todo.title;
+    todo.description = req.body.description || todo.description;
+    todo.status = req.body.status || todo.status;
+
+    res.json({ status: "success", todo });
+
+
+    res.json({ status: "success", todo });
+});
+app.delete("/todo/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const index = todos.findIndex(t => t.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ status: "error", message: "list was not found" });
+    }
+
+    todos.splice(index, 1);
+
+    res.json({ status: "success", message: "list deleted" });
+});
+app.listen(3000, () => {
+    console.log("Todo API running at http://localhost:3000");
+});
+
+
+
