062885f2b18a1588f5e7dd2a031af8857a8057c1
Haider Ali <haidet0000@gmail.com>
2025-12-05 16:31:08 +0500

    updated code of task9

--- task9/todo.js
+++ task9/todo.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-
 let todos = [];
 let nextId = 1;
 app.post("/todo", (req, res) => {
     const { title, description, status } = req.body;
+
     if (!title) {
         return res.status(400).json({ status: "error", message: "where is title" });
     }
     if (status !== "pending" && status !== "completed") {
-        return res.status(400).json({ status: "error", message: "status is  invalid" });
+        return res.status(400).json({ status: "error", message: "status is invalid" });
     }
     const newTodo = {
         id: nextId++,
@@ -47,12 +47,18 @@ app.put("/todo/:id", (req, res) => {
     if (!todo) {
         return res.status(404).json({ status: "error", message: "your todo list was not found" });
     }
+    if (req.body.status !== undefined) {
+        if (req.body.status !== "pending" && req.body.status !== "completed") {
+            return res.status(400).json({
+                status: "error",
+                message: "status is invalid"
+            });
+        }
+        todo.status = req.body.status;
+    }
+
     todo.title = req.body.title || todo.title;
     todo.description = req.body.description || todo.description;
-    todo.status = req.body.status || todo.status;
-
-    res.json({ status: "success", todo });
-
 
     res.json({ status: "success", todo });
 });
@@ -68,9 +74,7 @@ app.delete("/todo/:id", (req, res) => {
 
     res.json({ status: "success", message: "list deleted" });
 });
+
 app.listen(3000, () => {
     console.log("Todo API running at http://localhost:3000");
 });
-
-
-
